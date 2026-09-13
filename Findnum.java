26cba6cc40c9da7a1a81828042493efcf9dddd14
TIERPC <shaikhsahil21.8.3@gmail.com>
2026-09-13 22:33:20 +0530

    new

---
+++ Findnum.java
@@ -0,0 +1,13 @@
+public class Findnum {
+    public static void main (String[] args){
+        int arr[] = {25,45,36,98,12,11,22};
+        int n = arr.length;
+        for (int i = 0; i <n ; i++) {
+            if (arr[i]<35) {
+                System.out.println(i);
+
+            }
+
+        }
+    }
+}
